fcc17f07e9fd4cfb90df7298948958114660eed4
Phil de Joux <philderbeast@gmail.com>
2021-12-23 11:12:54 -0500

    Follow hlint suggestions except for CPP module.

--- src-ghc-8.6/Internal/Type.hs
+++ src-ghc-8.6/Internal/Type.hs
@@ -1,5 +1,6 @@
 module Internal.Type (substType) where
 
+import Data.Maybe (fromMaybe)
 import GhcPlugins hiding (mkSubst)
 import TcType (TcType)
 import TyCoRep (Type (..))
@@ -11,9 +12,8 @@ substType
   :: [(TcTyVar, TcType)]
   -> TcType
   -> TcType
-substType subst tv@(TyVarTy v) = case lookup v subst of
-  Just t  -> t
-  Nothing -> tv
+substType subst tv@(TyVarTy v) =
+  fromMaybe tv (lookup v subst)
 substType subst (AppTy t1 t2) =
   AppTy (substType subst t1) (substType subst t2)
 substType subst (TyConApp tc xs) =
--- src-ghc-9.2/Internal/Type.hs
+++ src-ghc-9.2/Internal/Type.hs
@@ -1,5 +1,6 @@
 module Internal.Type (substType) where
 
+import Data.Maybe (fromMaybe)
 import GHC.Tc.Utils.TcType (TcType)
 import GHC.Core.TyCo.Rep (Type (..))
 import GHC.Types.Var (TcTyVar)
@@ -11,9 +12,8 @@ substType
   :: [(TcTyVar, TcType)]
   -> TcType
   -> TcType
-substType subst tv@(TyVarTy v) = case lookup v subst of
-  Just t  -> t
-  Nothing -> tv
+substType subst tv@(TyVarTy v) =
+  fromMaybe tv (lookup v subst)
 substType subst (AppTy t1 t2) =
   AppTy (substType subst t1) (substType subst t2)
 substType subst (TyConApp tc xs) =
