6e2248d598fb25201ac858170872933185dec153
Phil de Joux <philderbeast@gmail.com>
2022-07-16 14:52:31 -0400

    Follow hlint suggestion: redundant bracket.

--- src-ghc-flat/Internal.hs
+++ src-ghc-flat/Internal.hs
@@ -142,8 +142,8 @@ mkSubst' = foldr substSubst [] . mapMaybe mkSubst
 
 -- | Create simple substitution from type equalities
 mkSubst :: Ct -> Maybe ((TcTyVar, TcType),Ct)
-mkSubst ct@(CTyEqCan {..})  = Just ((cc_tyvar,cc_rhs),ct)
-mkSubst ct@(CFunEqCan {..}) = Just ((cc_fsk,TyConApp cc_fun cc_tyargs),ct)
+mkSubst ct@CTyEqCan{..}  = Just ((cc_tyvar,cc_rhs),ct)
+mkSubst ct@CFunEqCan{..} = Just ((cc_fsk,TyConApp cc_fun cc_tyargs),ct)
 mkSubst _                   = Nothing
 
 -- | Apply substitution in the evidence of Cts
